e227c044a205271e23226c7af1bcf881a2463bdf
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-10-10 17:33:17 +0200

    added overload and timeit decorator

--- torchic/utils/overload.py
+++ torchic/utils/overload.py
@@ -1,33 +1,48 @@
 '''
-    Classes and functions used for function overloading
+    Freebooted from pythonlangutil
 '''
-import inspect
-
-class overload:
-    # This class will act as the decorator directly
-    functions = {}
-
+def signature(*types):
+    def func(f):
+        def inner_func(callingObj, *args, **kwargs):
+            if callingObj is None:
+                return f(*args, **kwargs)
+            else:
+                return f(callingObj, *args, **kwargs)
+        inner_func.signature = types
+        return inner_func
+    return func
+        
+class overload(object):
     def __init__(self, func):
-        self.func = func
-        self.register(func)
-
-    def register(self, func):
-        # Get the function signature (number and types of arguments)
-        signature = inspect.signature(func)
-        param_types = tuple(param.annotation for param in signature.parameters.values())
-
-        if func.__name__ not in overload.functions:
-            overload.functions[func.__name__] = {}
-
-        # Store function by its name and argument types
-        overload.functions[func.__name__][param_types] = func
-
+        self.owner = None
+        self.signatures = []
+        self.methods = []
+        self.methods.append(func)
+        self.signatures.append(func.signature)
+        
+    def __get__(self, owner, ownerType=None):
+        self.owner = owner or self
+        return self
+    
     def __call__(self, *args, **kwargs):
-        func_key = tuple(type(arg) for arg in args)
-        func_name = self.func.__name__
-
-        if func_key in overload.functions[func_name]:
-            # Dispatch to the appropriate overloaded function
-            return overload.functions[func_name][func_key](*args, **kwargs)
+        '''
+            To each version of the overloaded method, a signature is assigned. 
+            The signature is a list of types of the accepted arguments.
+            The __call__ method checks the signature of the arguments passed to the method and
+            calls the correct version of the overloaded method. 
+            NOTE: The signature does not consider kwargs.
+        '''
+        signature = []
+        for arg in args:
+            signature.append(arg.__class__.__name__)
+        signature = tuple(signature)
+        if signature in self.signatures:
+            index = self.signatures.index(signature)
         else:
-            raise TypeError(f"No matching function for arguments {func_key}")
+            raise Exception("There is no overload for this method with this signature.")
+        return self.methods[index](self.owner, *args, **kwargs)
+    
+    def overload(self, func):
+        self.methods.append(func)
+        self.signatures.append(func.signature)
+        return self
