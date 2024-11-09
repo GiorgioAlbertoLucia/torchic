3e6eb23610b7015cf1b1e36894fba909b6d23a91
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2024-11-09 16:57:55 +0100

    added dedicated folder for simulations

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torchic',
-    version='0.3.0',  # Initial version
+    version='0.4.2',  # Initial version
     author='Giorgio Alberto Lucia, Roberta Ferioli',
     author_email='giogioalberto@gmail.com',
     description='A Python library for data analysis in python and ROOT (tailored for high energy physics)',
@@ -15,7 +15,11 @@ setup(
         'License :: OSI Approved :: MIT License',  # Replace with your license
         'Operating System :: OS Independent',
     ],
-    package_data={'torchic': ['physics/BetheBloch.hh']},  # Include header files
+    package_data={'torchic': [
+        'physics/BetheBloch.hh',
+        'physics/simulations/ExponentialDecaySimulation.cpp',
+        'physics/simulations/TwoBodyDecaySimulation.cpp',
+        ]},  # Include header files
     python_requires='>=3.6',  # Minimum Python version
     install_requires=[
         # List your package dependencies here
