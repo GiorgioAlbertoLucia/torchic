e0a1d5c1fcb81162080c79dd8e3406ae740222cd
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2025-10-04 16:32:24 +0200

    added new roopdf

--- torchic/roopdf/__init__.py
+++ torchic/roopdf/__init__.py
@@ -10,12 +10,12 @@ def try_import_root():
 
         # Include headers and implementation
         gInterpreter.ProcessLine(f'#include "{pdf_dir}/RooGausExp.hh"')
-        gInterpreter.ProcessLine(f'#include "{pdf_dir}/RooGausExp.cxx"')
         gInterpreter.ProcessLine(f'#include "{pdf_dir}/RooSillPdf.hh"')
+        gInterpreter.ProcessLine(f'#include "{pdf_dir}/RooGausDExp.hh"')
 
         # Import the class to make it accessible
-        from ROOT import RooGausExp, RooSillPdf
-        return RooGausExp, RooSillPdf
+        from ROOT import RooGausExp, RooSillPdf, RooGausDExp
+        return RooGausExp, RooSillPdf, RooGausDExp
 
     except ImportError:
         print("ROOT not found. Functions will not be available.")
@@ -24,9 +24,10 @@ def try_import_root():
 
     return None, None
 
-RooGausExp, RooSillPdf = try_import_root()
+RooGausExp, RooSillPdf, RooGausDExp = try_import_root()
 
 __all__ = [
     'RooGausExp',
     'RooSillPdf',
+    'RooGausDExp',
 ]
