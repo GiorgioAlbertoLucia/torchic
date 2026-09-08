14a2816ede60d414f6a3baad9ee9f96f4d155d72
GiorgioAlbertoLucia <giogioalberto@gmail.com>
2026-09-08 10:13:21 +0200

    Add functionalities

--- torchic/core/plotter.py
+++ torchic/core/plotter.py
@@ -2,8 +2,10 @@
     Class to produce plots from given THn
 '''
 
-from ROOT import TCanvas, TFile, TLine, TBox, TLegend, TMultiGraph, TPad, TText
+from ROOT import TCanvas, TFile, TLine, TBox, TLegend, TMultiGraph, TPad, TText, TPaveText
 from ROOT import gStyle
+
+from torchic.utils.root import set_root_object, init_legend, set_alice_frame_style
 
 class Plotter:
 
@@ -17,6 +19,7 @@ class Plotter:
         self._hframe = None
         self.legends = []
         self.texts = []
+        self.pavetexts = {}
         self.multigraph = None
         
         self.hist_dict = {}
@@ -43,6 +46,23 @@ class Plotter:
         if 'top_margin' in kwargs:      self._canvas.SetTopMargin(kwargs['top_margin'])
         if 'bottom_margin' in kwargs:   self._canvas.SetBottomMargin(kwargs['bottom_margin'])
         self._hframe = self._canvas.DrawFrame(axis_specs[0]['xmin'], axis_specs[1]['xmin'], axis_specs[0]['xmax'], axis_specs[1]['xmax'], axis_specs[0]['title'])
+        if 'bin_labels' in axis_specs[0]:
+            n_bins = len(axis_specs[0]['bin_labels'])
+            self._hframe.GetXaxis().Set(n_bins, axis_specs[0]['xmin'], axis_specs[0]['xmax'])
+            for i, label in enumerate(axis_specs[0]['bin_labels']):
+                self._hframe.GetXaxis().SetBinLabel(i + 1, label)
+        if 'bin_labels' in axis_specs[1]:
+            n_bins = len(axis_specs[1]['bin_labels'])
+            self._hframe.GetYaxis().Set(n_bins, axis_specs[1]['xmin'], axis_specs[1]['xmax'])
+            for i, label in enumerate(axis_specs[1]['bin_labels']):
+                self._hframe.GetYaxis().SetBinLabel(i + 1, label)
+        if 'x_title_size' in kwargs: self._hframe.GetXaxis().SetTitleSize(kwargs['x_title_size'])
+        if 'y_title_size' in kwargs: self._hframe.GetYaxis().SetTitleSize(kwargs['y_title_size'])
+        if 'x_label_size' in kwargs: self._hframe.GetXaxis().SetLabelSize(kwargs['x_label_size'])
+        if 'y_label_size' in kwargs: self._hframe.GetYaxis().SetLabelSize(kwargs['y_label_size'])
+        if 'x_title_offset' in kwargs: self._hframe.GetXaxis().SetTitleOffset(kwargs['x_title_offset'])
+        if 'y_title_offset' in kwargs: self._hframe.GetYaxis().SetTitleOffset(kwargs['y_title_offset'])
+        #set_alice_frame_style(self._hframe, **kwargs)
 
         if kwargs.get('subplot_bottom', False):  
             self._n_pads = 2
@@ -71,50 +91,38 @@ class Plotter:
         else:
             self._pads[kwargs.get('draw_pad', 0)].cd()
         self.multigraph.Draw(kwargs.get('draw_option', 'SAME'))
+        
+    def _resolve_pad(self, **kwargs):
+        if self._n_pads > 1:
+            idx = kwargs.get('draw_pad', 0)
+            return idx, self._pads[idx]
+        return 0, self._canvas
+    
+    def _get_object(self, inpath:str, obj_name:str):
+        inFile = TFile(inpath, 'READ')
+        obj = inFile.Get(obj_name)
+        if 'TH' in str(type(obj)):
+            obj.SetDirectory(0)
+        inFile.Close()
+        return obj
     
     def add_hist(self, inpath:str, hist_name:str, hist_label:str, **kwargs):
 
-        inFile = TFile(inpath, 'READ')
-        hist = inFile.Get(hist_name)
-
-        hist.SetDirectory(0)
-        hist.SetLineColor(kwargs.get('line_color', 1))
-        hist.SetMarkerColor(kwargs.get('marker_color', 1))
-        hist.SetMarkerStyle(kwargs.get('marker_style', 20))
-        hist.SetMarkerSize(kwargs.get('marker_size', 1))
-        hist.SetLineWidth(kwargs.get('line_width', 1))
-        hist.SetLineStyle(kwargs.get('line_style', 1))
-        hist.SetFillColorAlpha(kwargs.get('fill_color', 0), kwargs.get('fill_alpha', 1))
-        hist.SetFillStyle(kwargs.get('fill_style', 0))
-        gStyle.SetPalette(kwargs.get('palette', 1))
+        hist = self._get_object(inpath, hist_name)
+        set_root_object(hist, **kwargs)
 
         self.hist_dict[hist_label] = hist
 
-        draw_pad_idx = 0
-        pad_to_draw = self._canvas
-        if self._n_pads > 1:
-            draw_pad_idx = kwargs.get('draw_pad', 0)
-            pad_to_draw = self._pads[draw_pad_idx]
+        draw_pad_idx, pad_to_draw = self._resolve_pad(**kwargs)
         
         if kwargs.get('leg_add', True) and self.legends[draw_pad_idx] is not None: self.legends[draw_pad_idx].AddEntry(self.hist_dict[hist_label], hist_label, kwargs.get('leg_option', 'fl'))
         pad_to_draw.cd()
         self.hist_dict[hist_label].Draw(kwargs.get('draw_option', 'SAME'))
-    
-        inFile.Close()
 
     def add_graph(self, inpath:str, graph_name:str, graph_label:str, **kwargs):
 
-        inFile = TFile(inpath, 'READ')
-        graph = inFile.Get(graph_name)
-
-        graph.SetFillColorAlpha(kwargs.get('fill_color', 0), kwargs.get('fill_alpha', 1))
-        graph.SetFillStyle(kwargs.get('fill_style', 0))
-        graph.SetLineColor(kwargs.get('line_color', 1))
-        graph.SetMarkerColor(kwargs.get('marker_color', 1))
-        graph.SetMarkerStyle(kwargs.get('marker_style', 20))
-        graph.SetMarkerSize(kwargs.get('marker_size', 1))
-        graph.SetLineWidth(kwargs.get('line_width', 1))
-        graph.SetLineStyle(kwargs.get('line_style', 1))
+        graph = self._get_object(inpath, graph_name)
+        set_root_object(graph, **kwargs)
 
         npoints = graph.GetN()
 
@@ -137,14 +145,10 @@ class Plotter:
         
         self.graph_dict[graph_label] = graph
 
-        if self._n_pads < 2:
-            if kwargs.get('leg_add', True) and self.legends[0] is not None: self.legends[0].AddEntry(self.graph_dict[graph_label], graph_label, kwargs.get('leg_option', 'p'))
-        else:
-            draw_pad_idx = kwargs.get('draw_pad', 0)
-            if kwargs.get('leg_add', True) and self.legends[draw_pad_idx] is not None: self.legends[draw_pad_idx].AddEntry(self.graph_dict[graph_label], graph_label, kwargs.get('leg_option', 'p'))
-        self.multigraph.Add(self.graph_dict[graph_label], kwargs.get('draw_option', 'SAME'))
+        draw_pad_idx, pad_to_draw = self._resolve_pad(**kwargs)
 
-        inFile.Close()
+        if kwargs.get('leg_add', True) and self.legends[draw_pad_idx] is not None: self.legends[draw_pad_idx].AddEntry(self.graph_dict[graph_label], graph_label, kwargs.get('leg_option', 'p'))
+        self.multigraph.Add(self.graph_dict[graph_label], kwargs.get('draw_option', 'SAME'))
 
     def add_func(self, inpath:str, func_name:str, func_label:str, **kwargs):
         '''
@@ -155,25 +159,16 @@ class Plotter:
             func_label: str
         '''
 
-        inFile = TFile(inpath, 'READ')
-        func = inFile.Get(func_name)
+        func = self._get_object(inpath, func_name)
+        set_root_object(func, **kwargs)
         
-        func.SetLineColor(kwargs.get('line_color', 1))
-        func.SetLineWidth(kwargs.get('line_width', 1))
-        func.SetLineStyle(kwargs.get('line_style', 1))
         self.func_dict[func_name] = func
 
-        draw_pad_idx = 0
-        pad_to_draw = self._canvas
-        if self._n_pads > 1:
-            draw_pad_idx = kwargs.get('draw_pad', 0)
-            pad_to_draw = self._pads[draw_pad_idx]
-        
+        draw_pad_idx, pad_to_draw = self._resolve_pad(**kwargs)
+
         if kwargs.get('leg_add', True) and self.legends[draw_pad_idx] is not None: self.legends[draw_pad_idx].AddEntry(self.func_dict[func_name], func_label, kwargs.get('leg_option', 'l'))
         pad_to_draw.cd()
         self.func_dict[func_name].Draw(kwargs.get('draw_option', 'SAME'))
-
-        inFile.Close()
 
     def add_ROI(self, line_specs: dict, box_specs: dict, **kwargs):
         '''
@@ -188,9 +183,7 @@ class Plotter:
         '''
         if type(line_specs) is dict:
             line = TLine(line_specs['x1'], line_specs['y1'], line_specs['x2'], line_specs['y2'])
-            line.SetLineColor(kwargs.get('line_color', 1))
-            line.SetLineWidth(kwargs.get('line_width', 1))
-            line.SetLineStyle(kwargs.get('line_style', 1))
+            set_root_object(line, **kwargs)
             self.line_dict[line_specs['name']] = line
             if kwargs.get('leg_add_line', True) and self.legends[0] is not None and 'name' in line_specs.keys(): 
                 self.legends[0].AddEntry(line, line_specs['name'], kwargs.get('leg_option', 'l'))
@@ -199,11 +192,7 @@ class Plotter:
         band.SetFillColorAlpha(kwargs.get('fill_color', 0), kwargs.get('fill_alpha', 1))
         band.SetFillStyle(kwargs.get('fill_style', 0))
 
-        draw_pad_idx = 0
-        pad_to_draw = self._canvas
-        if self._n_pads > 1:
-            draw_pad_idx = kwargs.get('draw_pad', 0)
-            pad_to_draw = self._pads[draw_pad_idx]
+        draw_pad_idx, pad_to_draw = self._resolve_pad(**kwargs)
 
         if 'name' in box_specs.keys():
             self.box_dict[box_specs['name']] = band
@@ -233,16 +222,10 @@ class Plotter:
         '''
         
         line = TLine(line_specs['x1'], line_specs['y1'], line_specs['x2'], line_specs['y2'])
-        line.SetLineColor(kwargs.get('line_color', 1))
-        line.SetLineWidth(kwargs.get('line_width', 1))
-        line.SetLineStyle(kwargs.get('line_style', 1))
+        set_root_object(line, **kwargs)
         self.line_dict[line_specs['name']] = line
         
-        draw_pad_idx = 0
-        pad_to_draw = self._canvas
-        if self._n_pads > 1:
-            draw_pad_idx = kwargs.get('draw_pad', 0)
-            pad_to_draw = self._pads[draw_pad_idx]
+        draw_pad_idx, pad_to_draw = self._resolve_pad(**kwargs)
 
         if kwargs.get('leg_add', True) and self.legends[draw_pad_idx] is not None: self.legends[draw_pad_idx].AddEntry(line, line_specs['name'], kwargs.get('leg_option', 'l'))
         pad_to_draw.cd()
@@ -262,12 +245,9 @@ class Plotter:
             self.legends.append(None)
             return 
         
-        legend = TLegend(position[0], position[1], position[2], position[3])
-        legend.SetHeader(kwargs.get('header', ''))
-        legend.SetBorderSize(kwargs.get('border_size', 0))
-        #legend.SetFillColor(kwargs.get('fill_color', 0))
-        #legend.SetFillStyle(kwargs.get('fill_style', 0))
-        #legend.SetTextSize(kwargs.get('text_size', 0.03))
+        kwargs.setdefault('border_size', 0)
+        kwargs.setdefault('fill_style', 0)
+        legend = init_legend(position[0], position[1], position[2], position[3], **kwargs)
 
         n_columns = kwargs.get('nColumns', 0)
         if n_columns != 0:
@@ -303,30 +283,84 @@ class Plotter:
             return
         
         text = TText(position[0], position[1], text)
-        text.SetTextSize(kwargs.get('text_size', 0.03))
-        text.SetTextAlign(kwargs.get('text_align', 11))
+        kwargs.setdefault('text_size', 0.03)
+        kwargs.setdefault('text_align', 11)
+        set_root_object(text, **kwargs)
+
         ipad = kwargs.get('draw_pad', 0)
         if self._n_pads > 1: self._pads[ipad].cd()
         else: self._canvas.cd()
         text.Draw()
         self.texts.append(text)
 
-    def _reset(self):
+    def add_pavetext(self, lines, position: list, label: str = None, **kwargs):
+        '''
+            Add a TPaveText to the plot
+
+            lines: str or list of str
+                single line or multiple lines of text
+            position: list
+                x1, y1, x2, y2: float (NDC by default)
+            label: str
+                key to store/retrieve this TPaveText, defaults to autoincrement
+            kwargs: dict
+                ndc: bool (default True) -> use NDC coordinates
+                border_size: int
+                fill_color: int
+                fill_style: int
+                text_align: int
+                text_size: float
+                text_font: int
+                text_color: int
+                draw_pad: int
+        '''
+        if not kwargs.get('bool', True):
+            return
+
+        option = 'NDC' if kwargs.get('ndc', True) else ''
+        pave = TPaveText(position[0], position[1], position[2], position[3], option)
+
+        if isinstance(lines, str):
+            lines = [lines]
+        for line in lines:
+            pave.AddText(line)
+        
+        kwargs.setdefault('border_size', 0)
+        kwargs.setdefault('fill_color', 0)
+        kwargs.setdefault('fill_style', 0)
+        kwargs.setdefault('text_size', 0.03)
+
+        set_root_object(pave, **kwargs)
+
+        key = label if label is not None else f'pavetext_{len(self.pavetexts)}'
+        self.pavetexts[key] = pave
+
+        ipad = kwargs.get('draw_pad', 0)
+        if self._n_pads > 1: self._pads[ipad].cd()
+        else: self._canvas.cd()
+        pave.Draw(kwargs.get('draw_option', 'SAME'))
+
+    def reset(self):
         self.hist_dict = {}
         self.graph_dict = {}
         self.line_dict = {}
         self.func_dict = {}
         self.box_dict = {}
+        self.texts = []
+        self.pavetexts = {}
         self._canvas.Clear()
         self._hframe = None
         self.legends = []
         self.multigraph = None 
+        self._pads = []
+        self._n_pads = 0
 
     def save(self, outPath:str):
         self._canvas.SaveAs(outPath)
         self.outfile.cd()
         self._canvas.Write()
-        self._reset()
+        self.reset()
         
     def close(self):
         self.outfile.Close()
+        
